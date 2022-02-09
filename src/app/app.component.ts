bf3e25095b41614a9b3e37582caf4a5c6b7a9c60
Alejandro Villanueva <alejonc15@email.com>
2022-02-08 23:10:31 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'any-question';
+  pregunta: string= '';
+  respuesta: string= '';
+  img: any = null;
+  isValid: boolean = false;
+
+  async getRespuesta(){
+    this.respuesta = 'Pensando...'
+    const { answer, image } = await fetch('https://yesno.wtf/api').then( r => r.json() )
+    console.log(answer);
+    console.log(image);
+    this.respuesta = answer === 'yes' ? 'Si!' : 'No!'
+    this.img = image
+    console.log(this.img);
+  }
+
+  onQuestion(event:any){
+    
+    this.isValid = false
+    
+    this.respuesta = event.target.value;
+
+    console.log(this.respuesta);
+    if( !this.respuesta.includes('?') ) return
+
+    this.isValid = true
+    // TODO: Realizar petición http
+    this.getRespuesta()
+  }
+
+
 }
